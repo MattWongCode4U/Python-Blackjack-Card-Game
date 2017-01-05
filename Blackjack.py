831c9a63a4273d2f4a5a29ce2376a481e9afc47b
Matthew Wong <matthewwong3.14@gmail.com>
2017-01-05 01:43:08 -0800

    Completed game logic, blackjack check and main game loop.

--- Blackjack.py
+++ Blackjack.py
@@ -1,41 +1,100 @@
 from deck_pkg.CardModule import CardDeck
 
+#Driver function
 def main():
     print("Starting up Blackjack Card Game")
-    gameDeck = CardDeck()
-    gameDeck.fillDeck()
-    gameDeck.shuffleDeck()
-    print("Cards in deck: ", gameDeck.getCardCount())
 
     print("\nBeginning Blackjack Card Game\n")
 
     playerAmount = getPlayerAmount()
 
-    #Game Setup
-    dealerHand = []
-    players = []
-    for i in range(playerAmount):
-        players.append([])
-
-    #Dealer draws
-    for i in range(2):
-        dealerHand.append(gameDeck.drawCard())
-
-    #Players draw
-    for i in range(playerAmount):
-            for j in range(2):
-                players[i].append(gameDeck.drawCard())
-
-##    printPlayingHands(dealerHand, players)
     gameRunning = True
 
+    #Main game loop
     while gameRunning:
+        #Game Setup
+        gameDeck = CardDeck()
+        gameDeck.fillDeck()
+        gameDeck.shuffleDeck()
+        print("Cards in deck: ", gameDeck.getCardCount())
+
+        dealerHand = []
+        players = []
+        for i in range(playerAmount):
+            players.append([])
+
+        #Dealer draws
+        for i in range(2):
+            dealerHand.append(gameDeck.drawCard())
+
+        #Players draw
+        for i in range(playerAmount):
+                for j in range(2):
+                    players[i].append(gameDeck.drawCard())
+                
         printPlayingHands(dealerHand, players)
 
+        #Players take their turn
         for i in range(playerAmount):
             playerTurn(players, i, gameDeck)
-            
+
+        #Dealer takes turn
+        dealerTurn(dealerHand, gameDeck)
+
+        #Display results of the round
+        roundResults(dealerHand, players)
+
+        #Ask if still playing
+        gameRunning = continuePlayCheck()
+
+#Print results of the round
+def roundResults(dealerHand, players):
+    print("***Round Over***")
+    print("Results:")
+    printPlayingHands(dealerHand, players)
     
+    if isSafeFromBust(sumValue(dealerHand)):
+        print("Dealer safe from bust")
+        for i in range(len(players)):
+            if isSafeFromBust(sumValue(players[i])):
+                if sumValue(players[i]) > sumValue(dealerHand):
+                    print("Player", i, " won!")
+                else:
+                    print("Player", i, " lost!")
+            else:
+                print("Player", i, " lost because they busted!")
+    else:
+        print("Dealer busted")
+        for i in range(len(players)):
+            if isSafeFromBust(sumValue(players[i])):
+                print("Player", i, " won!")
+            else:
+                print("Player", i, " lost because they busted!")
+
+#Determine if playing anther round or not  
+def continuePlayCheck():
+    while True:
+        playerInput = input("\nWould you like to play again? (Y/N)")
+        if playerInput.lower() == "y":
+            print("Restarting game.")
+            return True
+        elif playerInput.lower() == "n":
+            print("Thanks for playing! See you later!")
+            return False
+        else:
+            print("Unexpected input. Try Again!")
+            continue
+
+#Dealer turn logic
+def dealerTurn(dealerHand, gameDeck):
+    print("\n***Dealer Drawing***")
+    printDealerHand(dealerHand)
+    while(sumValue(dealerHand) < 17):
+        dealerHand.append(gameDeck.drawCard())
+        printDealerHand(dealerHand)
+    print()
+    
+#Get the amount of players playing
 def getPlayerAmount():
     choice = 0
     gettingInput = True
@@ -53,16 +112,30 @@ def getPlayerAmount():
             print("That's not a number")    
     return val
 
+#Player turn logic
 def playerTurn(players, index, deck):
     print("\nPlayer", index, "'s Turn")
     playing = True
     while playing:
         printPlayerHand(players, index)
         playerInput = input("Hit (H), Stay (S)")
+        #Hit
         if playerInput.lower() == "h":
             playing = True
             players[index].append(deck.drawCard())
-            continue
+            if(isSafeFromBust(sumValue(players[index]))):
+                if(sumValue(players[index]) == 21):
+                    print("BlackJack!")
+                    printPlayerHand(players, index)
+                    break
+                else:
+                    continue
+            else:
+                playing = False
+                print("Player", index, " busted!")
+                printPlayerHand(players, index)
+                break
+        #Stay
         elif playerInput.lower() == "s":
             playing = False
             break
@@ -70,6 +143,7 @@ def playerTurn(players, index, deck):
             print("Unexpected input, try again!")
             continue
 
+#Print dealer and players' hands
 def printPlayingHands(dealer, players):
     #print dealer's hand
     printDealerHand(dealer)
@@ -77,6 +151,7 @@ def printPlayingHands(dealer, players):
     #print player hands
     printPlayerHands(players)
 
+#Print dealer's hand
 def printDealerHand(dealer):
     dealerStr = ""
     for i in range(len(dealer)):
@@ -86,10 +161,12 @@ def printDealerHand(dealer):
     dealerStr += str(sumValue(dealer))
     print("Dealer's hand:", dealerStr)
 
+#Print all player hands
 def printPlayerHands(players):
     for i in range(len(players)):
         printPlayerHand(players, i)
 
+#Print a player's hand
 def printPlayerHand(players, index):
     playerStrTemp = ""
     playerStrTemp += "Player"
@@ -102,19 +179,21 @@ def printPlayerHand(players, index):
     playerStrTemp += str(sumValue(players[index]))
     print(playerStrTemp);
 
+#Calculate the value of a hand
 def sumValue(cardHand):
     tempsum = 0
     for i in range(len(cardHand)):
         tempsum += blackjackFaceValueConvert(cardHand[i].getRawValue())
-    finalSum = aceValueCheck(cardHand, tempsum)
-    return finalSum
+    return aceValueCheck(cardHand, tempsum)
 
+#Modifies face card values to 10
 def blackjackFaceValueConvert(value):
     if value > 10:
         return 10
     else:
         return value
 
+#Modifies the value of aces depending on the overall value
 def aceValueCheck(cardHand, tempsum):
     finalSum = tempsum
     for i in range(len(cardHand)):
@@ -123,12 +202,11 @@ def aceValueCheck(cardHand, tempsum):
                 finalSum += 10
     return finalSum
 
+#Check if the value is below 21
 def isSafeFromBust(value):
     if value <= 21:
         return True
     else:
         return False
-
-
 
 main()
